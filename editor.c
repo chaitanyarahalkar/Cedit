ae93d3bc3c075ced9a484e757cb1e4d903d5df28
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-10-28 21:47:04 +0530

    Add more key bindings

--- editor.c
+++ editor.c
@@ -16,7 +16,12 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY, 
+  PAGE_UP,
+  PAGE_DOWN
 
 };
 
@@ -87,26 +92,56 @@ int editorReadKey(){
 		char seq[3];
 
 		if(read(STDIN_FILENO, &seq[0],1) != 1)
-			return "\x1b";
+			return '\x1b';
 
 		if(read(STDIN_FILENO, &seq[1],1) != 1)
-			return "\x1b";
+			return '\x1b';
 
 		// To move cursor with arrow keys
 		if(seq[0] == '['){
 
+			if(seq[1] >= '0' && seq[1] <= '9'){
+				if(read(STDIN_FILENO,&seq[2],1) != 1)
+					return '\x1b';
+
+				if(seq[2] == '~'){
+
+					switch(seq[1]){
+						case '1': return HOME_KEY;
+						case '3': return DEL_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+					}
+				}
+			}
+			else{
+
+				switch(seq[1]){
+
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+
+				}
+			
+			}
+
+		}
+		else if(seq[0] == 'O'){
+
 			switch(seq[1]){
 
-				case 'A': return ARROW_UP;
-				case 'B': return ARROW_DOWN;
-				case 'C': return ARROW_RIGHT;
-				case 'D': return ARROW_LEFT;
-
-
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY; 
 			}
-		
 		}
-		return "\x1b";
+		return '\x1b';
 	}
 	else{
 
@@ -266,19 +301,24 @@ void editorMoveCursor(int key){
 	switch(key){
 
 		case ARROW_LEFT:
-			E.cx--;
+			// Handle cursor from going out of screen
+			if(E.cx != 0)
+				E.cx--;
 			break;
 
 		case ARROW_RIGHT:
-			E.cx++;
+			if(E.cx != E.screencols - 1)
+				E.cx++;
 			break;
 
 		case ARROW_UP:
-			E.cy--;
+			if(E.cy != 0)
+				E.cy--;
 			break;
 
 		case ARROW_DOWN:
-			E.cy++;
+			if(E.cy != E.screenrows - 1)
+				E.cy++;
 			break;
 
 	}
@@ -289,10 +329,31 @@ void editorProcessKeypress(){
 	int c = editorReadKey();
 
 	switch(c){
-		case CTRL_KEY('q'): 
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO,"\x1b[2J",4);
+			write(STDOUT_FILENO,"\x1b[H",3); 
 			exit(0); 
 			break;
 
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
+
+		case PAGE_UP:
+		case PAGE_DOWN:
+			/* Move cursor to top/bottom of screen 
+			   Code block to create time variable in switch statement
+			*/
+			{
+				int times = E.screenrows;
+				while(times--)
+					editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			}
+			break;
 		case ARROW_UP:
 		case ARROW_DOWN:
 		case ARROW_LEFT:
