080828ef46530948c5c515e10ca839028475ea4b
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-10-29 12:06:52 +0530

    Fix warning

--- editor.c
+++ editor.c
@@ -473,7 +473,7 @@ void editorDrawStatusBar(struct abuf *ab){
 	abAppend(ab, "\x1b[7m",4);
 
 	char status[80],rstatus[80];
-	int len = snprintf(status, sizeof(status), "%.20s - %d lines", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)": "");
+	int len = snprintf(status, sizeof(status), "%.20s - %d lines %s", E.filename ? E.filename : "[No Name]", E.numrows, E.dirty ? "(modified)": "");
 
 	int rlen = snprintf(rstatus, sizeof(rstatus), "%d%d",E.cy + 1, E.numrows);
 
