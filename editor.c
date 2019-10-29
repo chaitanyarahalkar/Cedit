ed322986b0e8ff0a1b13e3ef18a8f907f7568936
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-10-29 07:42:14 +0530

    Fix cursor scrolling issue

--- editor.c
+++ editor.c
@@ -396,16 +396,28 @@ void  editorRefreshScreen(){
 
 void editorMoveCursor(int key){
 
+	// Scroll to right
+	erow * row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
 	switch(key){
 
 		case ARROW_LEFT:
 			// Handle cursor from going out of screen
 			if(E.cx != 0)
 				E.cx--;
+			else if(E.cy > 0){
+				E.cy--;
+				E.cx = E.row[E.cy].size;
+			}
 			break;
 
 		case ARROW_RIGHT:
-			E.cx++;
+			if(row && E.cx < row->size)
+				E.cx++;
+			else if(row && E.cx == row->size){
+				E.cy++;
+				E.cx = 0;
+			}
 			break;
 
 		case ARROW_UP:
@@ -419,6 +431,12 @@ void editorMoveCursor(int key){
 			break;
 
 	}
+
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+
+	if(E.cx > rowlen)
+		E.cx = rowlen;
 
 }
 void editorProcessKeypress(){
