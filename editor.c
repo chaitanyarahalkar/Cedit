17d484073548be380a03d7d2a40f02f47fb34198
chaitanyarahalkar <chaitanyarahalkar4@gmail.com>
2019-10-30 12:32:09 +0530

    Fix minor search changes

--- editor.c
+++ editor.c
@@ -640,19 +640,51 @@ int editorRowRxToCx(erow *row, int rx) {
 // Incremental search 
 void editorFindCallback(char *query, int key){
 
-	if (key == '\r' || key == '\x1b')
-    	return;
 
+ // Searching in forward and backward direction
+	static int last_match = - 1;
+	static int direction = 1;
+
+	if (key == '\r' || key == '\x1b'){
+
+		last_match = -1;
+		direction = 1;
+    	return;
+	}
+	else if(key == ARROW_RIGHT || ARROW_DOWN)
+		direction = 1;
+
+	else if(key == ARROW_LEFT  || key == ARROW_UP)
+		direction = -1;
+
+	else{
+
+		last_match = -1;
+		direction =  1;
+	}
+
+	if(last_match == -1)
+		direction = 1;
+
+	int current = last_match;
 	int i;
 
 	for (i = 0; i < E.numrows; i++) {
 
-	    erow *row = &E.row[i];
+		current += direction;
+		if(current == -1)
+			current = E.numrows - 1;
+
+		else if(current == E.numrows)
+			current = 0;
+
+	    erow *row = &E.row[current];
 	    char *match = strstr(row->render, query);
 
 	    if (match) {
 
-	      E.cy = i;
+	      last_match = current;
+	      E.cy = current;
 	      E.cx = editorRowRxToCx(row, match - row->render);
 	      E.rowoff = E.numrows;
 	      break;
@@ -672,7 +704,7 @@ void editorFind(){
   	int saved_coloff = E.coloff;
   	int saved_rowoff = E.rowoff;
 
-	char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+	char *query = editorPrompt("Search: %s (Use ESC/Arrows/Enter)", editorFindCallback);
 
 	if(query)
 		free(query);
